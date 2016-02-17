5fd2d0bbf3bfc88e2acd7bee9011aed2b7d042b6
王猛 <meng.w@shulidata.com>
2016-02-17 18:26:01 +0800

    lombok 添加

--- web/public/src/main/java/com/linyou/controller/IndexController.java
+++ web/public/src/main/java/com/linyou/controller/IndexController.java
@@ -1,11 +1,20 @@
 package com.linyou.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by shuli on 16/2/17.
  */
+@Slf4j
 @Controller
+@RequestMapping("/")
 public class IndexController {
 
+    @RequestMapping("/index.html")
+    public String index(){
+
+        return "";
+    }
 }
